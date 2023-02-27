7e46ced8439dd15a001a81dcdb7ee06355cf39c5
Galaxyrapid1 <118562896+Galaxyrapid1@users.noreply.github.com>
2023-02-27 18:37:13 -0500

    multiplayer fixes

--- Tanks-3D/Assets/Scripts/Network/PlayerNetwork.cs
+++ Tanks-3D/Assets/Scripts/Network/PlayerNetwork.cs
@@ -5,20 +5,16 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    private readonly NetworkVariable<Vector3> _netPos = new(writePerm: NetworkVariableWritePermission.Owner);
-    private readonly NetworkVariable<Quaternion> _netRot = new(writePerm: NetworkVariableWritePermission.Owner);
-    
     void Update()
     {
-        if (IsOwner)
-        {
-            _netPos.Value = transform.position;
-            _netRot.Value = transform.rotation;
-        }
-        else
-        {
-            transform.position = _netPos.Value;
-            transform.rotation = _netRot.Value;
-        }
+        Vector3 moveDir = new Vector3(0, 0, 0);
+        
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 }
