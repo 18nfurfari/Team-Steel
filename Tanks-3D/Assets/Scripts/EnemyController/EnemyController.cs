ff5ecb32b796cad1d9cc9c81db8c229a8c1e81ed
Daflyingflank <120622490+Daflyingflank@users.noreply.github.com>
2023-02-27 17:25:37 -0500

    Added Firing to enemies
    Added Firing to enemies, Enemies can now do damage to the player

--- Tanks-3D/Assets/Scripts/EnemyController/EnemyController.cs
+++ Tanks-3D/Assets/Scripts/EnemyController/EnemyController.cs
@@ -10,23 +10,62 @@ public class EnemyController : MonoBehaviour
     Transform target;
     NavMeshAgent agent;
 
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+
+     public int currentAmmo;
+     private bool reloading;
+     private float reloadTime;
+
+    [SerializeField] private float bulletSpeed = 30f;
 
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+
+        reloading = false;
+        reloadTime = 3.0f;
+        currentAmmo = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(target != null)
+        {
         float distance = Vector3.Distance(target.position, transform.position);
+
+        if (reloading)
+        {
+            Reload();
+        }
+
         if (distance <= lookRange)
         {
         agent.SetDestination(target.position);
         if(distance <= agent.stoppingDistance)
             FaceTarget();
+        }
+        if (distance <= lookRange){
+                if(!reloading){
+                 var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position,
+                 bulletSpawnPoint.rotation * Quaternion.Euler(90, 0, 0));
+                 bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+
+                 Debug.Log("FIRE!");
+                 if (currentAmmo <= 0)
+                 {
+                    Debug.Log("Reloading!");
+                    reloading = true;
+                    reloadTime = 2.0f;
+                 }
+                 else{
+                    currentAmmo -= 1;
+                 }
+                 }
+          }
         }
     }
     void FaceTarget ()
@@ -35,6 +74,20 @@ public class EnemyController : MonoBehaviour
     Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
     transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime* 5f);
     }
+
+    private void Reload()
+        {
+            // Wait until waitTime is below or equal to zero.
+            if (reloadTime > 0)
+            {
+                reloadTime -= Time.deltaTime;
+            }
+            else
+            {
+                reloading = false;
+                currentAmmo = 1;
+            }
+        }
 
 
     void onDrawGizmosSelected ()
