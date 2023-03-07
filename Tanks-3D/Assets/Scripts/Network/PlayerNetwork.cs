308443ef4d796627a916d201cedc3d4e89400dc9
Galaxyrapid1 <118562896+Galaxyrapid1@users.noreply.github.com>
2023-03-07 17:15:34 -0500

    Networking Updates

--- Tanks-3D/Assets/Scripts/Network/PlayerNetwork.cs
+++ Tanks-3D/Assets/Scripts/Network/PlayerNetwork.cs
@@ -1,20 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    void Update()
-    {
-        Vector3 moveDir = new Vector3(0, 0, 0);
-        
-        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
-        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
-        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
-        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
-
-        float moveSpeed = 3f;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
-    }
+    // private void Update()
+    // {
+    //     if (!IsOwner)
+    //     {
+    //         PlayerInput.Destroy(this);
+    //     }
+    // }
 }
