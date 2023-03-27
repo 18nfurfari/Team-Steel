e899dc6f17642ad7ed400279e472585cdc9c78fb
18nfurfari <112352692+18nfurfari@users.noreply.github.com>
2023-03-27 01:37:48 -0400

    Player Compass, Top Down Camera, Deleted Some Unused Assets

--- Tanks-3D/Assets/Scripts/Compass.cs
+++ Tanks-3D/Assets/Scripts/Compass.cs
@@ -1,31 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Compass : MonoBehaviour
 {
-    public Transform player;
-    public Transform compassObject;
-    public Vector3 northDirection;
+    public Transform playerTransform;
+    private Vector3 _direction;
 
     void Update()
     {
-        Vector3 playerDirection = player.forward;
-        playerDirection.y = 0f;
-        Quaternion playerRotation = Quaternion.LookRotation(playerDirection);
-
-        Vector3 compassDirection = northDirection;
-        compassDirection.y = 0f;
-        Quaternion compassRotation = Quaternion.LookRotation(compassDirection);
-
-        float angle = Quaternion.Angle(playerRotation, compassRotation);
-        Vector3 cross = Vector3.Cross(playerRotation * Vector3.forward, compassRotation * Vector3.forward);
-        if (cross.y < 0)
-        {
-            angle = -angle;
-        }
-
-        compassObject.rotation = Quaternion.Euler(0f, 0f, -angle);
+        _direction.z = playerTransform.eulerAngles.y;
+        transform.localEulerAngles = _direction;
     }
 }
+
+
+
+
+
+
+
+
