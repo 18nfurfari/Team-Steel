4076edc735fdc74341393961baf409090c628dd3
Daflyingflank <120622490+Daflyingflank@users.noreply.github.com>
2023-04-10 17:15:18 -0400

    enemy network

---
+++ Tanks-3D/Assets/EnemyNetwork.cs
@@ -0,0 +1,46 @@
+using System;
+using TMPro;
+using Unity.Netcode;
+using Unity.Services.Lobbies.Models;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Object = UnityEngine.Object;
+using Quaternion = System.Numerics.Quaternion;
+using Random = UnityEngine.Random;
+
+public class EnemyNetwork : NetworkBehaviour
+{
+    [SerializeField] private Transform bulletPrefab;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float bulletSpeed = 30f;
+    [SerializeField] private float shootingRange = 10f;
+
+    private Transform _playerTransform;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer) return;
+
+        _playerTransform = FindObjectOfType<PlayerNetwork>().transform;
+
+        InvokeRepeating(nameof(ShootPlayer), 2f, 2f);
+    }
+
+
+    private void ShootPlayer()
+    {
+        if (!IsServer) return;
+
+        if (_playerTransform == null) return;
+
+        float distance = Vector3.Distance(transform.position, _playerTransform.position);
+
+        if (distance < shootingRange)
+        {
+            Transform bulletTransform = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation * UnityEngine.Quaternion.Euler(90, 0, 0));
+            bulletTransform.GetComponent<NetworkObject>().Spawn(true);
+            bulletTransform.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        }
+    }
+}
